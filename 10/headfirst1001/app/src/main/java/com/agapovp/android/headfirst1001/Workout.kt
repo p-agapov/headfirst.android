383911a6226be6747a50f77d787f025196a2065b
p-agapov <p.a.agapov@gmail.com>
2020-04-09 22:26:49 +0300

    1001 - complete

---
+++ 10/headfirst1001/app/src/main/java/com/agapovp/android/headfirst1001/Workout.kt
@@ -0,0 +1,44 @@
+package com.agapovp.android.headfirst1001
+
+class Workout private constructor(
+    val name: String,
+    val description: String
+) {
+    override fun toString(): String {
+        return this.name
+    }
+
+    companion object {
+        val workouts = arrayOf(
+            Workout(
+                "The Limb Loosener",
+                """5 Handstand push-ups
+                    |10 1-legged squats
+                    |15 Pull-ups
+                """.trimMargin()
+            ),
+            Workout(
+                "Core Agony",
+                """100 Pull-ups
+                    |100 Push-ups
+                    |100 Sit-ups
+                    |100 Squats
+                """.trimMargin()
+            ),
+            Workout(
+                "The Wimp Special",
+                """5 Pull-ups
+                    |10 Push-ups
+                    |15 Squats
+                """.trimMargin()
+            ),
+            Workout(
+                "Strength and Length",
+                """500 meter run
+                    |21 x 1.5 pood kettleball swing
+                    |21 x pull-ups
+                """.trimMargin()
+            )
+        )
+    }
+}
